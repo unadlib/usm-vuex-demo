bfcc9aefc9b42b90ad6aa6211bf1014627754427
Michael Lin <unadlib@gmail.com>
2019-04-14 13:59:07 +0800

    update

--- packages/app/src/shims-vue.d.ts
+++ packages/app/src/shims-vue.d.ts
@@ -1,9 +1,9 @@
+import Vue from "vue";
 declare module "*.vue" {
-  import Vue from "vue";
   export default Vue;
 }
 
-import Vue from "vue";
+// import Vue from "vue";
 import Portal from "./modules/Portal";
 
 declare module 'vue/types/vue' {

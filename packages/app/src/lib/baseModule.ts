4402967a442919e4196953862cad328314f2108c
Michael Lin <unadlib@gmail.com>
2019-04-15 23:32:36 +0800

    add vuex logger plugin

--- packages/app/src/lib/baseModule.ts
+++ packages/app/src/lib/baseModule.ts
@@ -1,6 +1,9 @@
 import Module, { state, action, computed } from "usm-vuex";
+import createLogger from 'vuex/dist/logger';
 
-class BaseModule extends Module {}
+class BaseModule extends Module {
+   plugins = [createLogger()];
+}
 
 export {
    BaseModule as default,

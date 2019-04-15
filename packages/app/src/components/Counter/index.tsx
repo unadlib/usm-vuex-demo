b3cd87feaae26ea85a87708b3564dc5d9841a3c1
Michael Lin <unadlib@gmail.com>
2019-04-15 15:01:16 +0800

    add Mixins example

--- packages/app/src/components/Counter/index.tsx
+++ packages/app/src/components/Counter/index.tsx
@@ -1,12 +1,17 @@
-import { Component, Vue, Prop } from "vue-property-decorator";
+import { Component, Prop, Mixins } from "vue-property-decorator";
+import { FoobarMixin } from '../../lib/mixins';
 import './style.scss';
 
 type Calculate = (sum: number) => void;
 
 @Component
-export default class CounterView extends Vue {
+export default class CounterView extends Mixins(FoobarMixin) {
   @Prop() count!: number;
-  @Prop(Function) calculate!: Calculate; 
+  @Prop(Function) calculate!: Calculate;
+  
+  created () {
+    console.log(this.mixinValue)
+  }
 
   render(){
     return (
---
+++ packages/app/src/lib/mixins.ts
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+// You can declare a mixin as the same style as components.
+@Component
+export class FoobarMixin extends Vue {
+  mixinValue = 'Hello'
+}

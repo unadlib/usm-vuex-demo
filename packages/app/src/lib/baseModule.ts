5a7e98a840871b4fcc86eab55166eef666a93b39
Michael Lin <unadlib@gmail.com>
2019-04-20 17:10:47 +0800

    add vuex-orm

--- packages/app/src/lib/baseModule.ts
+++ packages/app/src/lib/baseModule.ts
@@ -1,8 +1,23 @@
 import Module, { state, action, computed } from "usm-vuex";
 import createLogger from 'vuex/dist/logger';
+import VuexORM from '@vuex-orm/core';
+ 
+const database = new VuexORM.Database()
 
 class BaseModule extends Module {
-   plugins = [createLogger()];
+  constructor(...args: any[]) {
+    super(...args);
+    this.models.forEach((model: any) => {
+      database.register(model);
+      this[model.entity] = model;
+    });
+  }
+
+  get models() {
+    return [];
+  }
+
+  plugins = [createLogger(), VuexORM.install(database)];
 }
 
 export {
--- packages/app/src/modules/Counter/index.ts
+++ packages/app/src/modules/Counter/index.ts
@@ -1,5 +1,30 @@
 import { injectable } from "inversify";
+import { Model } from '@vuex-orm/core';
 import Module, { state, action, computed } from "../../lib/baseModule";
+
+class User extends Model {
+  static entity = 'users'
+  static fields () {
+    return {
+      id: this.attr(null),
+      name: this.attr(''),
+      email: this.attr('')
+    }
+  }
+}
+class Post extends Model {
+  static entity = 'posts' 
+  static fields () {
+    return {
+      id: this.attr(null),
+      user_id: this.attr(null),
+      title: this.attr(''),
+      body: this.attr(''),
+      published: this.attr(false),
+      author: this.belongsTo(User, 'user_id')
+    }
+  }
+}
 
 @injectable()
 export default class Counter extends Module {
@@ -15,5 +40,12 @@ export default class Counter extends Module {
       count: this.count,
       calculate: (num: number) => this.calculate(num)
     }
+  }
+
+  get models(): any {
+    return [
+      User,
+      Post
+    ]
   }
 }
